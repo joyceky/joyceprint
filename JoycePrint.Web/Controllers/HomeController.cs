9b937b03f2e4555af42ed8ddd8ce60b213900132
Kyle Joyce <kyyjoyce@gmail.com>
2017-02-19 15:59:21 -0700

    first commit

---
+++ JoycePrint.Web/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace JoycePrint.UI.Controllers
+{
+    [Route("/")]
+    public class HomeController : Controller
+    {        
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [Route("contact")]
+        [HttpGet]
+        public ActionResult ContactUs()
+        {
+            return View("ContactUs");
+        }
+
+        [Route("about")]
+        [HttpGet]
+        public ActionResult AboutUs()
+        {
+            return View("AboutUs");
+        }
+    }
+}
